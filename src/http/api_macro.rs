73a444e6ed9deff9af0f9821eec4488a33e4a32a
liuzsen <yjghdsss@gmail.com>
2025-07-02 19:54:11 +0800

    Refactor actix_api! macro to utilize HttpTypeProvider trait for improved type handling and clarity in HTTP request processing.

--- src/http/api_macro.rs
+++ src/http/api_macro.rs
@@ -1,41 +1,55 @@
+pub trait HttpTypeProvider {
+    type Adapter;
+    type UseCase;
+}
+
 #[macro_export]
 macro_rules! actix_api {
-    ($name:ident, $path0:ident $(::$path_seg:ident)*) => {
+    ($name:ident) => {
         pub async fn $name(
             req: actix_web::HttpRequest,
             payload: actix_web::web::Bytes,
         ) -> Result<actix_web::HttpResponse, lolibaso::http::error::HttpError> {
-            use lolibaso::http::Adapter;
+            use lolibaso::http::HttpAdapter;
+            use lolibaso::http::api_macro::HttpTypeProvider;
             use lolibaso::http::request::actix_impl::ActixHttpRequest;
             use lolibaso::provider::Provider;
             use lolibaso::use_case::UseCase;
 
-            type __UseCase = crate::infratructure::types:: $path0 $(::$path_seg)* ::UseCase;
-            type __JsonParser = crate::infratructure::types::JsonParser;
+            use self::inject::$name;
 
-            let req = ActixHttpRequest {
-                request: req,
-                payload,
-            };
+            type __Parser = crate::infratructure::types::Parser;
+            type __UseCase = <$name as HttpTypeProvider>::UseCase;
+            type __Adapter = <$name as HttpTypeProvider>::Adapter;
+
+            let req = ActixHttpRequest::new(req, payload);
+
             // get adapter
-            let adapter = crate::adapters::api_http:: $path0 $(::$path_seg)* ::Adapter::new();
+            let adapter = __Adapter::new();
 
             // convert input
-            let input = Adapter::<__UseCase, _>::convert_input(&adapter, req, __JsonParser::new())?;
+            let input =
+                HttpAdapter::<__UseCase, _>::convert_input(&adapter, &req, __Parser::provide()?)?;
 
             // do use case
             let use_case = __UseCase::provide()?;
-            let output = use_case.execute(input).await?;
 
+            // execute use case
+            let output = use_case.execute(input).await?;
             match output {
                 Ok(output) => {
                     // convert output
-                    let response = Adapter::<__UseCase, __JsonParser>::convert_output(&adapter, output);
-                    Ok(lolibaso::http::response::actix_impl::ToActixResponse::to_actix_response(response))
-                },
+                    let response =
+                        HttpAdapter::<__UseCase, __Parser>::convert_output(&adapter, output);
+                    Ok(
+                        lolibaso::http::response::actix_impl::ToActixResponse::to_actix_response(
+                            response,
+                        ),
+                    )
+                }
                 Err(err) => {
                     // convert error
-                    let err = Adapter::<__UseCase, __JsonParser>::convert_err(&adapter, err);
+                    let err = HttpAdapter::<__UseCase, __Parser>::convert_err(&adapter, err);
                     return Err(From::from(err));
                 }
             }
